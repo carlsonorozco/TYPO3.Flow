neos/flow-development-collection

[TASK] Make Mvc\Controller\Exception extend Mvc\Exception
Formerly the exception subclassed FLOW3\Exception directly.

Change-Id: Ibae6e0f4980d94e1f4fa5c169f5ca225b52d539f
Resolves: #42785
Releases: 1.1, 1.2
Original-Commit-Hash: 2c27eb19fc8c6ba777c2a542bebebc3c6fffa8c7

TYPO3.Flow/Classes/TYPO3/Flow/Mvc/Controller/Exception.php
@@ -16,7 +16,7 @@ namespace TYPO3\Flow\Mvc\Controller;
  *
  * @api
  */
-class Exception extends \TYPO3\Flow\Exception {
+class Exception extends \TYPO3\Flow\Mvc\Exception {
 
 	/**
 	 * @var \TYPO3\Flow\Mvc\RequestInterface

carlsonorozco/TYPO3.Flow

Classes/Mvc/Controller/Exception.php
@@ -16,7 +16,7 @@ namespace TYPO3\FLOW3\Mvc\Controller;
  *
  * @api
  */
-class Exception extends \TYPO3\FLOW3\Exception {
+class Exception extends \TYPO3\FLOW3\Mvc\Exception {
 
 	/**
 	 * @var \TYPO3\FLOW3\Mvc\RequestInterface